1c1ff72b6afefbc93a4d4f372f352240a9a2f54d
Bryant Moreira dos Anjos <bryant_mx@hotmail.com>
2020-03-29 14:24:31 -0300

    Did the details page

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,11 +1,70 @@
 import React from 'react'
+import { Image, TouchableOpacity } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import Icon from 'react-native-vector-icons/Feather'
 
-import { View } from 'react-native'
+import {
+  Container,
+  Header,
+  Incident,
+  IncidentProperty,
+  IncidentValue,
+  ContactBox,
+  HeroTitle,
+  HeroDescription,
+  Actions,
+  Action,
+  ActionText,
+} from './styles'
 
-// import { Container } from './styles';
+import logoImg from '../../assets/logo.png'
 
 const Detail = () => {
-  return <View />
+  const navigation = useNavigation()
+
+  function navigateBack() {
+    navigation.goBack()
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Image source={logoImg} />
+
+        <TouchableOpacity onPress={navigateBack}>
+          <Icon name="arrow-left" size={28} color="#e82041" />
+        </TouchableOpacity>
+      </Header>
+
+      <Incident>
+        <IncidentProperty style={{ marginTop: 0 }}>ONG:</IncidentProperty>
+        <IncidentValue>APAD</IncidentValue>
+
+        <IncidentProperty>CASO:</IncidentProperty>
+        <IncidentValue>Cadelinha atropelada</IncidentValue>
+
+        <IncidentProperty>VALOR:</IncidentProperty>
+        <IncidentValue>R$ 120,00</IncidentValue>
+      </Incident>
+
+      <ContactBox>
+        <HeroTitle>Salve o dia!</HeroTitle>
+        <HeroTitle>Seja o herói desse caso.</HeroTitle>
+
+        <HeroDescription>Entre em contato:</HeroDescription>
+
+        <Actions>
+          <Action onPress={() => {}}>
+            <ActionText>WhatsApp</ActionText>
+          </Action>
+
+          <Action onPress={() => {}}>
+            <ActionText>E-mail</ActionText>
+          </Action>
+        </Actions>
+      </ContactBox>
+    </Container>
+  )
 }
 
 export default Detail
