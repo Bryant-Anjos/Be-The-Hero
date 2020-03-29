96140502993da2b6d92cba58458e0b62d30a1d9e
Bryant Moreira dos Anjos <bryant_mx@hotmail.com>
2020-03-29 13:56:20 -0300

    Did the incidents page

--- mobile/src/pages/Incidents/index.tsx
+++ mobile/src/pages/Incidents/index.tsx
@@ -1,11 +1,68 @@
 import React from 'react'
+import { Image } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import Icon from 'react-native-vector-icons/Feather'
 
-import { View } from 'react-native'
+import {
+  Container,
+  Header,
+  HeaderText,
+  HeaderTextBold,
+  Title,
+  Description,
+  IncidentList,
+  Incident,
+  IncidentProperty,
+  IncidentValue,
+  DetailsButton,
+  DetailsButtonText,
+} from './styles'
 
-// import { Container } from './styles';
+import logoImg from '../../assets/logo.png'
 
 const Incidents = () => {
-  return <View />
+  const navigation = useNavigation()
+
+  function navigateToDetail() {
+    navigation.navigate('Detail')
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Image source={logoImg} />
+        <HeaderText>
+          Total de <HeaderTextBold>0 casos</HeaderTextBold>.
+        </HeaderText>
+      </Header>
+
+      <Title>Bem vindo!</Title>
+      <Description>Escolha um dos caso abaixo e salve o dia.</Description>
+
+      <IncidentList
+        data={[1, 2, 3, 4, 5]}
+        keyExtractor={(incident) => String(incident)}
+        showsVerticalScrollIndicator={false}
+        renderItem={() => (
+          <Incident>
+            <IncidentProperty>ONG:</IncidentProperty>
+            <IncidentValue>APAD</IncidentValue>
+
+            <IncidentProperty>CASO:</IncidentProperty>
+            <IncidentValue>Cadelinha atropelada</IncidentValue>
+
+            <IncidentProperty>VALOR:</IncidentProperty>
+            <IncidentValue>R$ 120,00</IncidentValue>
+
+            <DetailsButton onPress={navigateToDetail}>
+              <DetailsButtonText>Ver mais detalhes</DetailsButtonText>
+              <Icon name="arrow-right" size={16} color="#e02041" />
+            </DetailsButton>
+          </Incident>
+        )}
+      />
+    </Container>
+  )
 }
 
 export default Incidents
